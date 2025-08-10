16316e22411835dc806991884665a2309410225a
MustafaGood <mustafa.swedish@gmail.com>
2025-08-10 20:53:00 +0200

    Lägg till AnimatedBackground-komponent
    - Flytande stjärnor för visuell effekt
    - Skjutande stjärnor för dynamik
    - Flytande moln för extra djup i bakgrunden

---
+++ frontend/src/components/AnimatedBackground.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AnimatedBackground: React.FC = () => {
+  return (
+    <>
+      {/* Floating Stars */}
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      
+      {/* Shooting Stars */}
+      <div className="shooting-star"></div>
+      <div className="shooting-star"></div>
+      
+      {/* Floating Clouds */}
+      <div className="cloud"></div>
+      <div className="cloud"></div>
+    </>
+  );
+};
+
+export default AnimatedBackground;
